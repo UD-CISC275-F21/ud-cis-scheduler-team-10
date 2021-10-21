e9532f4dee863bd161b02db704676facc2f23306
Alex Pilling <apilling@udel.edu>
2021-10-21 18:44:50 -0400

    Changed course input into semester table

--- src/components/SemesterTab.tsx
+++ src/components/SemesterTab.tsx
@@ -2,23 +2,38 @@ import React from "react";
 import { Tab, Tabs } from "react-bootstrap";
 import { ClearSemester } from "./ClearSemester";
 import { SemesterTable } from "./SemesterTable";
+import CourseData from "../data/courses.json";
+import { Course } from "../interfaces/Course";
 
+
+
+const courseArray = JSON.parse(JSON.stringify(CourseData));
 
 export function SemesterTab(): JSX.Element {
     return (
         <Tabs defaultActiveKey="semester_1" id="uncontrolled-tab-example" className="mb-3">
             <Tab eventKey="semester_1" title="Semester 1" >
-                <SemesterTable></SemesterTable>
+                <SemesterTable course1={courseArray[0] as Course} course2={courseArray[1] as Course} course3={courseArray[2] as Course}></SemesterTable>
                 <ClearSemester></ClearSemester>
             </Tab>
             <Tab eventKey="semester_2" title="Semester 2">
+                <SemesterTable course1={courseArray[3] as Course} course2={courseArray[4] as Course} course3={courseArray[5] as Course}></SemesterTable>
+                <ClearSemester></ClearSemester>
+            </Tab>
+            <Tab eventKey="semester_3" title="Semester 3">
+                <SemesterTable course1={courseArray[6] as Course} course2={courseArray[7] as Course} course3={courseArray[8] as Course}></SemesterTable>
+                <ClearSemester></ClearSemester>
+            </Tab>
+            
+        </Tabs>
+    );
+}
+
+/*<Tab eventKey="semester_2" title="Semester 2">
                 <SemesterTable></SemesterTable>
                 <ClearSemester></ClearSemester>
             </Tab>
             <Tab eventKey="semester_3" title="Semester 3">
                 <SemesterTable></SemesterTable>
                 <ClearSemester></ClearSemester>
-            </Tab>
-        </Tabs>
-    );
-}
+            </Tab>*/
--- src/components/SemesterTable.tsx
+++ src/components/SemesterTable.tsx
@@ -1,36 +1,32 @@
 import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 import InlineEdit from "./InlineEdit";
+import { Course } from "../interfaces/Course";
 
 
-export function SemesterTable (): JSX.Element{
+export function SemesterTable ({course1, course2, course3}: {course1: Course, course2: Course, course3: Course}): JSX.Element{
 
-    const [storedCourseNumber1, setStoredCourseNumber1] = useState("CISC108");
-    const [storedCourseCredit1, setStoredCourseCredit1] = useState("3");
-    const [storedCourseName1, setStoredCourseName1] = useState("Introduction to Programming");
-    const [storedCourseDes1, setStoredCourseDes1] = useState(
-        `Computing and principles of programming with an emphasis on systematic program design. Topics include functional programming, data abstraction, procedural abstraction, 
-        use of control and state, recursion, testing, and object-oriented programming concepts. Requires no prior programming experience, open to any major, 
-        but intended primarily for majors and minors in computer science or mathematics.`);
-
-    const [storedCourseNumber2, setStoredCourseNumber2] = useState("MATH241");
-    const [storedCourseCredit2, setStoredCourseCredit2] = useState("4");
-    const [storedCourseName2, setStoredCourseName2] = useState("Analytic Geometry and Calculus A");
-    const [storedCourseDes2, setStoredCourseDes2] = useState(
-        `Functions, limits, continuity, derivatives. Polynomial, rational, exponential, hyperbolic, logarithmic, trigonometric and inverse trigonometric functions. 
-        Definite and indefinite integrals and the Fundamental Theorem of Calculus. Simple differential equations (separable ODE, linear ODE). ODE models leading to exponential growth and decay.`);
+    const [storedCourseNumber1, setStoredCourseNumber1] = useState(course1.Number);
+    const [storedCourseCredit1, setStoredCourseCredit1] = useState(course1.Credits);
+    const [storedCourseName1, setStoredCourseName1] = useState(course1.Name);
+    const [storedCourseDes1, setStoredCourseDes1] = useState(course1.Description);
     
-    const [storedCourseNumber3, setStoredCourseNumber3] = useState("EGGG101");
-    const [storedCourseCredit3, setStoredCourseCredit3] = useState("2");
-    const [storedCourseName3, setStoredCourseName3] = useState("Introduction to Engineering");
-    const [storedCourseDes3, setStoredCourseDes3] = useState(
-        `Introduction to profession, including disciplines of chemical, civil, computer, electrical, environmental, and mechanical engineering. 
-        Prepares students for success through integration of: technical problem solving and engineering design, ethical decision-making, teamwork, and communicating to diverse audiences.`);
-
+    const [storedCourseNumber2, setStoredCourseNumber2] = useState(course2.Number);
+    const [storedCourseCredit2, setStoredCourseCredit2] = useState(course2.Credits);
+    const [storedCourseName2, setStoredCourseName2] = useState(course2.Name);
+    const [storedCourseDes2, setStoredCourseDes2] = useState(course2.Description);
+    
+    const [storedCourseNumber3, setStoredCourseNumber3] = useState(course3.Number);
+    const [storedCourseCredit3, setStoredCourseCredit3] = useState(course3.Credits);
+    const [storedCourseName3, setStoredCourseName3] = useState(course3.Name);
+    const [storedCourseDes3, setStoredCourseDes3] = useState(course3.Description);
+    /*
     const [storedCourseNumber4, setStoredCourseNumber4] = useState("ENGL110");
     const [storedCourseCredit4, setStoredCourseCredit4] = useState("3");
     const [storedCourseName4, setStoredCourseName4] = useState("Seminar and Composition");
     const [storedCourseDes4, setStoredCourseDes4] = useState("Introduction to the process of academic writing that centers on the composition of analytical, research-based essays.");
+*/
+
 
     return(
         <Table striped 
@@ -64,13 +60,28 @@ export function SemesterTable (): JSX.Element{
                     <td><InlineEdit text={storedCourseCredit3} onSetText={text => setStoredCourseCredit3(text)}/></td>
                     <td><InlineEdit text={storedCourseDes3} onSetText={text => setStoredCourseDes3(text)}/></td>
                 </tr>
+
+                
+            </tbody>
+        </Table>
+    );
+}
+
+/*<tr>
+                    <td><InlineEdit text={storedCourseNumber2} onSetText={text => setStoredCourseNumber2(text)}/></td>
+                    <td><InlineEdit text={storedCourseName2} onSetText={text => setStoredCourseName2(text)}/></td>
+                    <td><InlineEdit text={storedCourseCredit2} onSetText={text => setStoredCourseCredit2(text)}/></td>
+                    <td><InlineEdit text={storedCourseDes2} onSetText={text => setStoredCourseDes2(text)}/></td>
+                </tr>
+                <tr>
+                    <td><InlineEdit text={storedCourseNumber3} onSetText={text => setStoredCourseNumber3(text)}/></td>
+                    <td><InlineEdit text={storedCourseName3} onSetText={text => setStoredCourseName3(text)}/></td>
+                    <td><InlineEdit text={storedCourseCredit3} onSetText={text => setStoredCourseCredit3(text)}/></td>
+                    <td><InlineEdit text={storedCourseDes3} onSetText={text => setStoredCourseDes3(text)}/></td>
+                </tr>
                 <tr>
                     <td><InlineEdit text={storedCourseNumber4} onSetText={text => setStoredCourseNumber4(text)}/></td>
                     <td><InlineEdit text={storedCourseName4} onSetText={text => setStoredCourseName4(text)}/></td>
                     <td><InlineEdit text={storedCourseCredit4} onSetText={text => setStoredCourseCredit4(text)}/></td>
                     <td><InlineEdit text={storedCourseDes4} onSetText={text => setStoredCourseDes4(text)}/></td>
-                </tr>
-            </tbody>
-        </Table>
-    );
-}
+                </tr>*/
