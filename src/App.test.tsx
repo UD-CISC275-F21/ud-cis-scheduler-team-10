8f606c933050b6389220c37b24784ab31f947eb7
Alex Pilling <apilling@udel.edu>
2021-11-09 17:41:25 -0500

    removed unused import

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
-import userEvent from "@testing-library/user-event";
 
 test("renders UD CIS Scheduler text", () => {
     render(<App />);
