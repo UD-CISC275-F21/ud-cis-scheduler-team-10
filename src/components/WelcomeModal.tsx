ea183a5396288154ae94ec7744490d4cf4350be1
Anthony Racioppo <ajrac@udel.edu>
2021-12-08 22:58:51 -0500

    Styling, updated welcome modal

--- src/components/WelcomeModal.tsx
+++ src/components/WelcomeModal.tsx
@@ -16,10 +16,12 @@ export function WelcomeModal(): JSX.Element{
             <Modal.Body>Welcome! This site should be a nifty resource for students, advisors, and teachers to set up a course schedule.<p></p> You can make multiplie
                         schedules for future semesters if you wish by using the tabs at the top, or you can just stick to one semester.
                         You can get started by taking a look at the course list down below, and clicking on the &quot;Edit Course&quot; in the scheduler to edit it to your liking.
+                        If you want, you can add a course directly to a semester using the button in the Course list.
                         You can also add courses using the green &quot;Add Course button&quot;, clear specific courses using the &quot;Clear Course&quot; button, or 
                         clear an entire semester using the &quot;Remove all Courses&quot; button. <p></p>
                         You can also add and remove specific semesters using the &quot;Add Semester&quot; and the red X respectively, as well as remove
-                        all semesters, save your progress in your browser, or download the courselist to a CSV file using the respective buttons below.
+                        all semesters, save your progress in your browser, or download the courselist to a CSV file using the respective buttons below. Check out the 
+                        degree requirements using the button at the top, and make sure to check your pre-requisites tab whenever you pick a course.
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
